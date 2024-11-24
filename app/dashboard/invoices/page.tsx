fa74e34394ae841b995f5b650b68032411a1f6c7
kuramasa <kuramasa.green@gmail.com>
2024-11-24 11:03:49 +0900

    test commit

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -17,7 +17,7 @@ export default async function Page(props: {
   const query = searchParams?.query || ''
   const currentPage = Number(searchParams?.page) || 1
   const totalPages = await fetchInvoicesPages(query)
-
+  //test
   return (
     <div className='w-full'>
       <div className='flex w-full items-center justify-between'>
